f8fc0c9dc742e7d2fc94f524d68759513a39a89e
nnwas <taewarding909@gmail.com>
2026-07-11 09:36:57 -0400

    Update FurryPawsClient for Delta executor compatibility

--- FurryPawsClient.lua
+++ FurryPawsClient.lua
@@ -1,176 +1,52 @@
 --// Furry Paws FE Client System
 --// Final Connector
+--// Delta Executor Compatible
 
-local Players =
-	game:GetService("Players")
+local Players = game:GetService("Players")
+local Player = Players.LocalPlayer
+local Character = Player.Character or Player.CharacterAdded:Wait()
 
+local Modules = script:WaitForChild("Modules")
 
-local Player =
-	Players.LocalPlayer
-
-
-
-local Character =
-	Player.Character
-	or Player.CharacterAdded:Wait()
-
-
-
-local Modules =
-	script:WaitForChild(
-		"Modules"
-	)
-
-
-
-local Config =
-	require(
-		Modules.Config
-	)
-
-
-local PawBuilder =
-	require(
-		Modules.PawBuilder
-	)
-
-
-local PawAnimator =
-	require(
-		Modules.PawAnimator
-	)
-
-
-local PawPrints =
-	require(
-		Modules.PawPrints
-	)
-
-
-local Menu =
-	require(
-		Modules.Menu
-	)
-
-
-local Glow =
-	require(
-		Modules.Glow
-	)
-
-
-local Fluff =
-	require(
-		Modules.Fluff
-	)
-
-
-local Sounds =
-	require(
-		Modules.Sounds
-	)
-
-
-
-
+local Config = require(Modules.Config)
+local PawBuilder = require(Modules.PawBuilder)
+local PawAnimator = require(Modules.PawAnimator)
+local PawPrints = require(Modules.PawPrints)
+local Menu = require(Modules.Menu)
+local Glow = require(Modules.Glow)
+local Fluff = require(Modules.Fluff)
+local Sounds = require(Modules.Sounds)
 
 local function LoadPaws()
-
-
 	-- Create paws
-
-	local paws =
-		PawBuilder.Create(
-			Character,
-			Config
-		)
-
-
-
+	local paws = PawBuilder.Create(Character, Config)
+	
 	-- Animation
-
-	PawAnimator.Start(
-		Character,
-		paws,
-		Config
-	)
-
-
-
+	PawAnimator.Start(Character, paws, Config)
+	
 	-- Paw prints
-
-	PawPrints.Start(
-		Character,
-		Config
-	)
-
-
-
+	PawPrints.Start(Character, Config)
+	
 	-- Glow
-
-	Glow.Enable(
-		paws,
-		Config
-	)
-
-
-
+	Glow.Enable(paws, Config)
+	
 	-- Fur
-
-	Fluff.Create(
-		Character,
-		Config
-	)
-
-
-
+	Fluff.Create(Character, Config)
+	
 	-- Sounds
-
-	Sounds.Start(
-		Character,
-		Config
-	)
-
-
-
+	Sounds.Start(Character, Config)
+	
 	-- Menu
-
-	Menu.Create(
-		Config,
-		paws
-	)
-
-
-
-	print(
-		"🐾 Furry Paws FE Loaded Successfully!"
-	)
-
-
+	Menu.Create(Config, paws)
+	
+	print("🐾 Furry Paws FE Loaded Successfully!")
 end
-
-
-
-
 
 LoadPaws()
 
-
-
 -- Reload after respawn
-
-Player.CharacterAdded:Connect(
-function(char)
-
-
+Player.CharacterAdded:Connect(function(char)
 	task.wait(1)
-
-
-	Character =
-		char
-
-
+	Character = char
 	LoadPaws()
-
-
 end)
